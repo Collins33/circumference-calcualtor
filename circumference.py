d4523f8b1211bb38985e7d3570b8a8213065d7d6
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-08-07 17:55:51 +0300

    I have added the formula to calculate circumference using a random number

--- circumference.py
+++ circumference.py
@@ -1,1 +1,8 @@
 #calculate circumference of a circle
+import random
+#circumference is 2 pie r
+#r is a random number
+pie = 3.14
+radius = random.randint(0,100)
+circumference = 2 * pie * radius
+print(circumference)
